518ccc3160458defce179371978954057e9b9992
Spatchy <spatchy@spatchy.net>
2023-06-24 17:42:23 +0100

    Implement web browser with elegant plugin engine

---
+++ src/browser/components/Webview.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import { WebView } from "react-native-webview"
+import { StyleSheet } from "react-native"
+import ReddiSteddiCore from "../../plugins/ReddiSteddiCore.raw.js"
+
+export default function Webview() {
+  return (
+    <WebView
+      style={styles.container}
+      source={{ uri: "https://reddit.com" }}
+      injectedJavaScript={`${ReddiSteddiCore}`}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+})
