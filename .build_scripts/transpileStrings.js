7922db7669569ad451fdb487831a15d8450884de
Spatchy <spatchy@spatchy.net>
2023-07-01 23:04:57 +0100

    Implement build pipeline for .raw.css files

--- .build_scripts/transpileStrings.js
+++ .build_scripts/transpileStrings.js
@@ -3,12 +3,11 @@ const path = require("path")
 const jsStringEscape = require("js-string-escape")
 
 const templatePath = "exportStringTemplate.js"
+const cssTemplatePath = "exportCssStringTemplate.js"
 const rootDirectoryPath = "../build/src/"
 
-const templateContent = fs.readFileSync(templatePath, "utf8")
-
 console.log("The following raw files were transpiled to strings:")
-// Recursively traverse directories and process .raw.js files
+// Recursively traverse directories and process .raw.js and .raw.css files
 const traverseDirectory = (directoryPath) => {
   fs.readdirSync(directoryPath).forEach((filename) => {
     const filePath = path.join(directoryPath, filename)
@@ -16,12 +15,13 @@ const traverseDirectory = (directoryPath) => {
 
     if (stat.isDirectory()) {
       traverseDirectory(filePath)
-    } else if (stat.isFile() && filename.endsWith(".raw.js")) {
+    } else if (stat.isFile() && (filename.endsWith(".raw.js") || filename.endsWith(".raw.css"))) {
       console.log("    " + filename)
       let fileContent = fs.readFileSync(filePath, "utf8")
+      const templateContent = fs.readFileSync(filename.endsWith(".raw.js") ? templatePath : cssTemplatePath, "utf8")
       fileContent = jsStringEscape(fileContent)
       const modifiedContent = templateContent.replace("{{{STRINGIFIED_FILE_CONTENT}}}", fileContent)
-      fs.writeFileSync(filePath, modifiedContent, "utf8")
+      fs.writeFileSync(filePath.replace(".raw.css", ".raw.css.js"), modifiedContent, "utf8")
     }
   })
 }
--- declarations.d.ts
+++ declarations.d.ts
@@ -2,3 +2,8 @@ declare module "*.raw.js" {
   const contents: string
   export default contents
 }
+
+declare module "*.raw.css" {
+  const contents: string
+  export default contents
+}
